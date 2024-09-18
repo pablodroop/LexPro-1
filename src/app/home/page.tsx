0217a10cbdb107ef6dffd131ef0a1a3dde38b30d
pablodroop <pablodroop@gmail.com>
2024-09-18 10:58:06 -0300

    teste1

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>asdasdasdasdasd</h1>
+//     </div>
+//   )
+// }
+export default function Hero () {
   return (
-    <div>
-      <h1>asdasdasdasdasd</h1>
-    </div>
+      <div className="w-full h-36 relative flex justify-between bg-gray800 px-8 tablet:px-[8.5rem] items-center">
+          <img
+              src="bg.png"
+              className="absolute object-cover opacity-40 h-36  w-full inset-0"
+          />
+          <h1 className="text-5xl font-semibold text-brown400 ">Sua Empresa protegida, crescimento garantido!</h1>
+      </div>
   )
 }
